6f9c46234dc5de89f365c1900781bb351a3a5a75
Pedro <pedroaraujo_41@hotmail.com>
2022-03-11 16:56:18 +0000

    Initial commit

---
+++ app/src/main/java/com/example/aula3_turno3/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.aula3_turno3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        Toast.makeText(this,R.string.hello, Toast.LENGTH_LONG).show()
+    }
+}
